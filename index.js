5297cf0f278b44f043c9ec248aa74a12b453189f
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-03 04:03:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function showForkedRepo(res){
 
 function getIssues() {
   fetch(`${gh}/${repo}/issues`)
-    .then(res => res.json()).then(json => $("#issues").html(showIssues(json)))
+    .then(res => res.json()).then(showIssues(json))
 }
 
 function showIssues(json) {
